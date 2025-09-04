048075bb8399a2cd7d4ce1226e0d78135e9d96b2
Alexander Reinthal <email@reinthal.me>
2025-09-04 22:55:28 +0200

    fix: clean up menu

--- devenv.nix
+++ devenv.nix
@@ -16,10 +16,22 @@
     ruby.enable = true;
     ruby.bundler.enable = true;
   };
+  scripts.menu.exec = ''
+    echo
+    echo "DATADRIVET DOCS"
+    echo -e '> \033[32mmenu\033[0m - print this menu'
+    echo
+    echo 'Available Commands'
+    echo -e '> \033[32msetup-hooks\033[0m - setup the githooks.'
+    echo -e '> \033[32mbuild\033[0m - builds the develpoment server'
+    echo -e '> \033[32mserve\033[0m - start the development server'
+    echo
+    echo 'ENVIRONMENT VARIABLES'
+  '';
   scripts.serve.exec = ''
     bundle exec jekyll serve --host 0.0.0.0 --port 4000
   '';
-  scripts.build-jekyll.exec = ''bundle exec jekyll build'';
+  scripts.build.exec = ''bundle exec jekyll build'';
   scripts.setup-hooks.exec = ''
     echo -e "\033[33mSetting up pre-commit hooks...\033[0m"
     pre-commit install
@@ -28,13 +40,15 @@
   '';
 
   enterShell = ''
-    build-jekyll
+    bundle install
+    build
     # Setup pre-commit hooks
     if [ ! -f .git/hooks/pre-commit ] || [ ! -f .git/hooks/commit-msg ]; then
      setup-hooks
     else
       echo -e "\033[32mPre-commit hooks already installed ✓\033[0m"
     fi
+    menu
   '';
 
   # https://devenv.sh/tasks/
